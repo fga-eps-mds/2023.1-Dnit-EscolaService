dd25e2ffd3043f2764237496560da1ba35c736ee
lucas229 <lucasro229@gmail.com>
2023-06-18 15:21:54 -0300

    #16 Ajustes no service e controller para implementação dos testes

---
+++ service/SmtpClientWrapper.cs
@@ -0,0 +1,34 @@
+﻿using service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public class SmtpClientWrapper : ISmtpClientWrapper
+    {
+        private SmtpClient _smtpClient;
+
+        public SmtpClientWrapper()
+        {
+            string emailRemetente = Environment.GetEnvironmentVariable("EMAIL_SERVICE_ADDRESS");
+            string senhaRemetente = Environment.GetEnvironmentVariable("EMAIL_SERVICE_PASSWORD");
+
+            _smtpClient = new SmtpClient("smtp-mail.outlook.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(emailRemetente, senhaRemetente),
+                EnableSsl = true,
+            };
+        }
+
+        public void Send(MailMessage message)
+        {
+            _smtpClient.Send(message);
+        }
+    }
+}
