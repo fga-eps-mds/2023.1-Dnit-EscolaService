a76a4e1165c3b80184085eec487d0f1ca3b57d1c
lucas229 <lucasro229@gmail.com>
2023-06-18 15:25:56 -0300

    #16 Adicionando testes para SolicitacaoAcaoService

---
+++ test/Stub/SolicitacaoAcaoStub.cs
@@ -0,0 +1,22 @@
+﻿using dominio;
+
+namespace test.Stub
+{
+    public class SolicitacaoAcaoStub
+    {
+        public SolicitacaoAcaoDTO ObterSolicitacaoAcaoDTO()
+        {
+            return new SolicitacaoAcaoDTO
+            {
+                Escola = "Escola Teste",
+                NomeSolicitante = "João Testador",
+                VinculoEscola = "Professor",
+                Email = "joao@email.com",
+                Telefone = "123123123",
+                CiclosEnsino = new[] { "Ensino Médio", "Ensino Fundamental" },
+                QuantidadeAlunos = 503,
+                Observacoes = "Teste de Solicitação"
+            };
+        }
+    }
+}
