9c386c3e19f011f1de5a73a17c8b0be15d4288d9
avmricardo <ricardomaciel.unb@gmail.com>
2023-06-18 22:20:30 -0300

    #12 cadastro de escola por arquivo csv, 1a versao
    Co-authored-by: Artur <artur190503@outlook.com>

--- service/EscolaService.cs
+++ service/EscolaService.cs
@@ -30,9 +30,36 @@ namespace service
                     parser.TextFieldType = FieldType.Delimited;
                     parser.SetDelimiters(";");
 
+                    bool primeiralinha = false;
+
                     while (!parser.EndOfData)
                     {
                         string[] linha = parser.ReadFields();
+                        if (!primeiralinha)
+                        {
+                            primeiralinha = true;
+                            continue;
+                        }
+                        Escola escola = new Escola();
+                        escola.NomeEscola = linha[0];
+                        escola.CodigoEscola = int.Parse(linha[1]); 
+                        escola.Cep = linha[2];
+                        escola.Endereco = linha[3];
+                        escola.Latitude = linha[4];
+                        escola.Longitude = linha[5];
+                        escola.NumeroTotalDeAlunos = int.Parse(linha[6]);
+                        escola.Telefone = linha[7];
+                        escola.NumeroTotalDeDocentes = int.Parse(linha[8]);
+                        escola.IdEscola = int.Parse(linha[9]);
+                        escola.IdRede = int.Parse(linha[10]);
+                        escola.IdUf = int.Parse(linha[11]);
+                        escola.IdLocalizacao = int.Parse(linha[12]);
+                        escola.IdMunicipio = int.Parse(linha[13]);
+                        escola.IdEtapasDeEnsino = int.Parse(linha[14]);
+                        escola.IdPorte = int.Parse(linha[15]);
+                        escola.IdSituacao = int.Parse(linha[16]);
+
+                        escolaRepositorio.CadastrarEscola(escola);
                     }
                 }
             }
