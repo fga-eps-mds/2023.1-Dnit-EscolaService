6d58e858ac240658625cb0c5ce5a828e5625b13c
Letícia Resende <letigy78@gmail.com>
2023-06-21 23:22:51 -0300

    #5 implementando dropdown
    Co-authored-by: DenysRogeres <dennysrogeres@gmail.com>
    Co-authored-by: JuliaSSouza <juliasantanna.souza@gmail.com>

---
+++ repositorio/DominioRepositorio.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using dominio.Dominio;
+using dominio.Enums;
+using repositorio.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static repositorio.Contexto.ResolverContexto;
+
+namespace repositorio
+{
+    public class DominioRepositorio
+    {
+        private readonly IContexto contexto;
+
+        public DominioRepositorio(ResolverContextoDelegate resolverContexto)
+        {
+            contexto = resolverContexto(ContextoBancoDeDados.Postgresql);
+        }
+        public IEnumerable<UnidadeFederativa> ObterUnidadeFederativa()
+        {
+            var sql = @"SELECT id, sigla, descricao FROM public.unidade_federativa";
+
+            var unidadesFederativas = contexto?.Conexao.Query<UnidadeFederativa>(sql);
+
+            return unidadesFederativas ?? Enumerable.Empty<UnidadeFederativa>();
+        }
+        public IEnumerable<EtapasdeEnsino> ObterEtapasdeEnsino()
+        {
+            var sql = @"SELECT id, sigla, descricao FROM public.etapas_de_ensino";
+
+            var EtapasdeEnsino = contexto?.Conexao.Query<EtapasdeEnsino>(sql);
+
+            return EtapasdeEnsino ?? Enumerable.Empty<EtapasdeEnsino>();
+        }
+        public IEnumerable<Municipio> ObterMunicipio()
+        {
+            var sql = @"SELECT id, sigla, descricao FROM public.municipio";
+
+            var Municipio = contexto?.Conexao.Query<Municipio>(sql);
+
+            return Municipio ?? Enumerable.Empty<Municipio>();
+        }
+        public IEnumerable<Situacao> ObterSituacao()
+        {
+            var sql = @"SELECT id, sigla, descricao FROM public.situcao";
+
+            var Situacao = contexto?.Conexao.Query<Situacao>(sql);
+
+            return Situacao ?? Enumerable.Empty<Situacao>();
+        }
+
+    }
+}
