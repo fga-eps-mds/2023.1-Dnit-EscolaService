146ec5b746c3b93094da8d63d829bfa38c8643f8
MateusPy <mateusorlando2014@gmail.com>
2023-06-25 16:24:40 -0300

    #6 Teste do Cadastro da Escola Service
    Co-authored-by: migueldefrias <miguelfrias8@gmail.com>
    Co-authored-by: lucas229 <lucasro229@gmail.com>

---
+++ test/EscolaServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Moq;
+using repositorio.Interfaces;
+using service;
+using dominio;
+using service.Interfaces;
+
+namespace test
+{
+    public class EscolaServiceTest
+    {
+        [Fact]
+        public void CadastrarEscola_QuandoForChamado_DeveChamarORepositorioUmaVez()
+        {
+            Mock<IEscolaRepositorio> mockEscolaRepositorio = new();
+            IEscolaService escolaService = new EscolaService(mockEscolaRepositorio.Object);
+            CadastroEscolaDTO cadastroEscolaDTO = new();
+
+            escolaService.CadastrarEscola(cadastroEscolaDTO);
+            mockEscolaRepositorio.Verify(x => x.CadastrarEscola(cadastroEscolaDTO), Times.Once);
+        }
+
+    }
+}
