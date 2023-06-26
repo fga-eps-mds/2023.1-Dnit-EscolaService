1a07dbe1ce7ddd2ab0fa0b617bc524c4daee25bb
Carolina Barbosa <carolinabarbosab09@gmail.com>
2023-06-25 21:56:07 -0300

    Co-authored-by: GuilhermeSoaress <guilhermesoares278@gmail.com> Co-authered-by: DenysRogeres <dennysrogeres@gmail.com>

--- test/EscolaRepositorioTest.cs
+++ test/EscolaRepositorioTest.cs
@@ -41,5 +41,42 @@ namespace test
             escolaService.Listar(IdEscola);
             mockEscolaRepositorio.Verify(x => x.Obter(IdEscola), Times.Once);
         }
+        
+        [Fact]
+        public void RemoverSituacaoEscola_QuandoOIdForInexistente_DeveRetornarErro()
+        {
+            Mock<IEscolaRepositorio> mockEscolaRepositorio = new();
+            IEscolaService escolaService = new EscolaService(mockEscolaRepositorio.Object);
+            var IdEscola = 8;
+            var IdInexistente = 999;
+
+            escolaService.RemoverSituacaoEscola(IdInexistente);
+            mockEscolaRepositorio.Verify(x => x.RemoverSituacaoEscola(IdInexistente),Times.Once);
+        }
+
+         [Fact]
+        public void AdicionarSituacaoEscola_QuandoOIdForInexistente_DeveRetornarErro()
+        {
+            Mock<IEscolaRepositorio> mockEscolaRepositorio = new();
+            IEscolaService escolaService = new EscolaService(mockEscolaRepositorio.Object);
+            AtualizarSituacaoDTO atualizarSituacaoDto = new() {IdSituacao = 7, IdEscola = 4};
+            int IdInexistente = 3;
+
+            escolaService.AdicionarSituacao(atualizarSituacaoDto);
+            mockEscolaRepositorio.Verify(x => x.AdicionarSituacao(atualizarSituacaoDto.IdSituacao, IdInexistente),Times.Never);
+        }
+
+         [Fact]
+        public void Listar_QuandoOIdInexistenteForChamado_DeveRetornarErro()
+        {
+            Mock<IEscolaRepositorio> mockEscolaRepositorio = new();
+            IEscolaService escolaService = new EscolaService(mockEscolaRepositorio.Object);
+            var IdEscola = 22;
+            var IdInexistente = 15;
+
+            escolaService.Listar(IdEscola);
+            mockEscolaRepositorio.Verify(x => x.Obter(IdInexistente), Times.Never);
+        }
+
     }
 }
