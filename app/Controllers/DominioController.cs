b3a7a54b876c952914d3a5a749216bf4a64f7e0f
Cássio Reis <csreis04@gmail.com>
2023-10-20 10:45:36 -0300

    feat: adiciona permissões de usuário referentes ao serviço de escolas

--- app/Controllers/DominioController.cs
+++ app/Controllers/DominioController.cs
@@ -1,19 +1,10 @@
 ﻿using dominio;
 using repositorio;
 using Microsoft.AspNetCore.Mvc;
-using repositorio;
 using repositorio.Interfaces;
 using service;
 using service.Interfaces;
 using dominio.Dominio;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using Microsoft.AspNetCore.Authorization;
-using app.Services;
-using System.ComponentModel;
 
 namespace app.Controllers
 {
@@ -22,41 +13,16 @@ namespace app.Controllers
     public class DominioController : ControllerBase
     {
         private readonly IDominioRepositorio dominioRepositorio;
-        private readonly IConfiguration configuration;
-        private readonly AuthService authService;
 
-        public DominioController(IDominioRepositorio dominioRepositorio, IConfiguration configuration, AuthService authService)
+
+        public DominioController(IDominioRepositorio dominioRepositorio)
         {
             this.dominioRepositorio = dominioRepositorio;
-            this.configuration = configuration;
-            this.authService = authService;
-        }
-
-        [HttpGet("login/teste")]
-        public IResult LoginTeste(string username, string password)
-        {
-            if (username != "joydip" || password != "joydip123")
-            {
-                return Results.Unauthorized();
-            }
-            var (token, expiracao) = authService.GenerateToken(new auth.AuthUserModel<Permissao>
-            {
-                Id = 12,
-                Name = "Cassio",
-                Permissions = new List<Permissao>() { Permissao.VisualizarUnidadeFederativa},
-            }) ;
-            return Results.Ok(token);
-        }
-        public enum Permissao {
-            [Description("Visualizar Unidades Federativas")]
-            VisualizarUnidadeFederativa = 5000,
         }
 
         [HttpGet("unidadeFederativa")]
-        [Authorize]
         public IActionResult ObterListaUF()
         {
-            authService.Require(User, Permissao.VisualizarUnidadeFederativa);
             IEnumerable<UnidadeFederativa> listaUnidadeFederativa = dominioRepositorio.ObterUnidadeFederativa();
             return new OkObjectResult(listaUnidadeFederativa);
         }
--- app/Controllers/EscolaController.cs
+++ app/Controllers/EscolaController.cs
@@ -1,8 +1,10 @@
+using app.Services;
 using dominio;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using service;
 using service.Interfaces;
-
+using System.ComponentModel;
 
 namespace app.Controllers
 {
@@ -11,17 +13,51 @@ namespace app.Controllers
     public class EscolaController : ControllerBase
     {
         private readonly IEscolaService escolaService;
+        private readonly AuthService authService;
 
-        public EscolaController(IEscolaService escolaService)
+        public EscolaController(IEscolaService escolaService, AuthService authService)
         {
             this.escolaService = escolaService;
+            this.authService = authService;
         }
 
+        public enum Permissao
+        {
+            [Description("Cadastrar Escola")]
+            EscolaCadastrar = 1000,
+
+            [Description("Editar Escola")]
+            EscolaEditar = 1001,
+
+            [Description("Remover Escola")]
+            EscolaRemover = 1002,
+
+            [Description("Visualizar Escola")]
+            EscolaVisualizar = 1003,
+        }
+
+        [HttpGet("login/teste")]
+        public IResult LoginTeste(string username, string password)
+        {
+            if (username != "joydip" || password != "joydip123")
+            {
+                return Results.Unauthorized();
+            }
+            var (token, expiracao) = authService.GenerateToken(new auth.AuthUserModel<Permissao>
+            {
+                Id = 12,
+                Name = "Cassio",
+                Permissions = new List<Permissao>() { Permissao.EscolaVisualizar, Permissao.EscolaRemover, Permissao.EscolaCadastrar },
+            });
+            return Results.Ok(token);
+        }
+
+        [Authorize]
         [Consumes("multipart/form-data")]
         [HttpPost("cadastrarEscolaPlanilha")]
         public async Task<IActionResult> EnviarPlanilha(IFormFile arquivo)
         {
-
+            authService.Require(User, Permissao.EscolaCadastrar);
             List<string> escolasNovas;
 
             try
@@ -60,38 +96,50 @@ namespace app.Controllers
             }
         }
 
+        [Authorize]
         [HttpGet("obter")]
         public IActionResult ObterEscolas([FromQuery] PesquisaEscolaFiltro pesquisaEscolaFiltro)
         {
+            authService.Require(User, Permissao.EscolaVisualizar);
+
             ListaPaginada<EscolaCorreta> listaEscolaPaginada = escolaService.Obter(pesquisaEscolaFiltro);
 
             return new OkObjectResult(listaEscolaPaginada);
         }
+
+        [Authorize]
         [HttpDelete("excluir")]
         public IActionResult ExcluirEscola([FromQuery] int id)
         {
+            authService.Require(User, Permissao.EscolaRemover);
             escolaService.ExcluirEscola(id);
             return Ok();
-
         }
 
+        [Authorize]
         [HttpPost("cadastrarEscola")]
         public IActionResult CadastrarEscola([FromBody] CadastroEscolaDTO cadastroEscolaDTO)
         {
+            authService.Require(User, Permissao.EscolaCadastrar);
             escolaService.CadastrarEscola(cadastroEscolaDTO);
             return Ok();
         }
 
+        [Authorize]
         [HttpPost("removerSituacao")]
         public IActionResult RemoverSituacao([FromQuery] int idEscola)
         {
+            authService.Require(User, Permissao.EscolaRemover);
             escolaService.RemoverSituacaoEscola(idEscola);
             return Ok();
         }
 
+        [Authorize]
         [HttpPut("alterarDadosEscola")]
         public IActionResult AlterarDadosEscola([FromBody] AtualizarDadosEscolaDTO atualizarDadosEscolaDTO)
         {
+            authService.Require(User, Permissao.EscolaEditar);
+
             try
             {
                 escolaService.AlterarDadosEscola(atualizarDadosEscolaDTO);
