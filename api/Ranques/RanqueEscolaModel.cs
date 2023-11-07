0c02384b9c1c8763d43045f0bac6d82fd3c149b4
DiceRunner714 <eduardo.sandes6@gmail.com>
2023-11-07 20:31:23 -0300

    feat: adicionando RanqueController
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>
    Co-authored-by: Artur Bartz <arturbylly2@gmail.com>

---
+++ api/Ranques/RanqueEscolaModel.cs
@@ -0,0 +1,9 @@
+namespace api.Ranques
+{
+    public class RanqueEscolaModel
+    {
+        public Guid IdEscola { get; set; }
+        public string Nome { get; set; }
+        public int Pontuacao { get; set; }
+    }
+}
