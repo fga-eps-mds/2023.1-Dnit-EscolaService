7f8d48dc88781ab1351041da5c7a616882bd8ec9
Yudi Yamane <yudiyamane@gmail.com>
2023-11-14 02:41:33 -0300

    chore: rascunho de testes

---
+++ test/RanqueControllerTest.cs
@@ -0,0 +1,32 @@
+﻿
+namespace test
+{
+    public class RanqueControllerTest
+    {
+        public RanqueControllerTest() { }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void CalcularNovoRanqueAsync_QuandoNaoHouverRanques_RetornaRanqueVazio()
+        {
+            // Na real eu não sei o que deve retonar de vdd
+        }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void CalcularNovoRanqueAsync_QuandoJaTemRanqueEmProcessamento_RetornaExcessao()
+        {
+            // Na real eu não sei o que deve acontecee de vdd
+        }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void CalcularNovoRanqueAsync_QuandoTudoNormal_DeveAtualizarUpsDasEscolasNoBancoDe_dados()
+        {
+            // Na real eu não sei o que deve acontecee de vdd
+        }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void CalcularRanque_DeveSerAutenticado()
+        {
+            // Na real eu não sei o que deve retonar de vdd
+        }
+    }
+}
---
+++ test/RanqueServiceTest.cs
@@ -0,0 +1,22 @@
+﻿
+namespace test
+{
+    public class RanqueServiceTest
+    {
+        public RanqueServiceTest() { }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void RequisicaoHttpParaUpsServiceDeveSerAutenticado() { }
+
+        [Fact(Skip = "Apenas rascunhando")]
+        public void ObterEscolaEmRanqueDetalhes()
+        {
+            // endpoint do olhozinho. O que deve retornar ?
+        }
+
+        // [Fact(Skip = "Apenas rascunhando")]
+        // public void ___()
+        // {
+        // }
+    }
+}
